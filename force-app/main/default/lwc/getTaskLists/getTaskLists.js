f477f2d565573c372a27f2c110ad210e78c72c93
Morna Gurivindapalli <dhananjaysfdc@gmail.com>
2022-07-22 19:17:59 +0400

    updated logic

--- force-app/main/default/lwc/getTaskLists/getTaskLists.js
+++ force-app/main/default/lwc/getTaskLists/getTaskLists.js
@@ -2,52 +2,47 @@ import { LightningElement,wire,track} from 'lwc';
 import gettaskliststodisplay from '@salesforce/apex/DisplayTasksController.gettaskliststodisplay';
 
 export default class getTaskLists extends LightningElement {
-    @track items = []; //this will hold key, value pair
-    @track value = ''; //initialize combo box value
-    @track chosenValue = '';
-    @track updateditems;
-    @wire(gettaskliststodisplay)
-     wiredRecords({ error, data }) {
-        window.console.log('before data====='+data);
-    if (data) {
-           window.console.log('data====='+JSON.stringify(data));
-        //create array with elements which has been retrieved controller
-        //here value will be record Id and label of combobox will be Name of record
-      //  for(let i=0; i<data.length; i++)  {
-        //     this.items = [...this.items ,{value: data[i].Id , label: data[i].Name} ];                                   
-        //} 
-        for(let i=0; i<data.length; i++)  {
-            console.log('i==='+data[i].groupnames);
-            this.ops =  [
-                { label: data[i].groupnames, value: data[i].groupnames }
-              ];
-              this.opslist = this.ops;                                  
-       }  
-       console.log('ops==='+this.ops);   
-       console.log('ops==='+JSON.stringify(this.opslist));   
-       this.updateditems = this.opslist; 
-        window.console.log('this.items== after for'+JSON.stringify(this.updateditems));        
-        this.error = undefined;
-    } else if (error) {
-        this.error = error;
-        window.console.log('this.errors=='+this.error);
-    }
+@track items = []; //this will hold key, value pair
+@track value = ''; //initialize combo box value
+@track chosenValue = '';
+@track updateditems;
+@wire(gettaskliststodisplay)
+wiredRecords({ error, data }) {
+
+if (data) {
+    
+//create array with elements which has been retrieved controller
+//here value will be record Id and label of combobox will be Name of record
+
+this.options = [];
+for(let i=0; i<data.length; i++)  {
+    
+    
+        this.options.push({ label: data[i].groupnames, value: data[i].groupnames });
+                                        
+}  
+this.updateditems = this.options;      
+this.error = undefined;
+} else if (error) {
+this.error = error;
+window.console.log('this.errors=='+this.error);
+}
 }
 
 get recordOptions() {
-   return this.updateditems;                        
-   }
+return this.updateditems;                        
+}
 
-              handleChange(event) {
-         // Get the string of the "value" attribute on the selected option
-         alert('test');
-        const selectedOption = event.detail.value;
-    console.log('selected value=' + selectedOption);
-    this.chosenValue = selectedOption;
+        handleChange(event) {
+    // Get the string of the "value" attribute on the selected option
+
+const selectedOption = event.detail.value;
+
+this.chosenValue = selectedOption;
 }
 
 //this value will be shown as selected value of combobox item
 get selectedValue(){
-    return this.chosenValue;
+return this.chosenValue;
 }
 }
