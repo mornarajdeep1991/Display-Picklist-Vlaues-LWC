2b0beeae5d3d22a598004db2a95573025a4c9c77
Morna Gurivindapalli <dhananjaysfdc@gmail.com>
2022-07-25 13:11:20 +0400

    updated with the record redirection

--- force-app/main/default/lwc/displayTasks/displayTasks.js
+++ force-app/main/default/lwc/displayTasks/displayTasks.js
@@ -1,37 +1,49 @@
 import { api, LightningElement, wire, track } from 'lwc';
-const columns = [
-    { label: 'id', fieldName: 'id' },
-    {
-        label: 'subject',
-        fieldName: 'subject',
-        type: 'text',
-        sortable: true,
-        cellAttributes: { alignment: 'center' },
-    },
-    { label: 'owner', fieldName: 'owner', type: 'text' }
-];
+
 import displaytasks from '@salesforce/apex/DisplayTasksController.displaytasks';
 export default class DisplayTasks extends LightningElement {
-    @track updateditems;
-    @api taskOwnerName;
-    @track items;
-    
-    @wire(displaytasks,{ownerValue: '$taskOwnerName'})
+@track dataWrapperList;
+@api taskOwnerName;
+@track updateditems;
+@track columns = [
+    { label: 'Task Name', fieldName: 'tksubject',type:'url',typeAttributes: {label: { fieldName: 'subject' }, target: '_blank'} },
+    {
+        label: 'Description',
+        fieldName: 'Description',
+        type: 'text',
+        sortable: true,
+        cellAttributes: { alignment: 'left' },
+    },
+    { label: 'Owner', fieldName: 'owner', type: 'text' }
+];
+@track data;
+@wire(displaytasks,{ownerValue: '$taskOwnerName'})
 wiredRecords({ error, data }) {
 
 if(data){
 console.log('data======'+JSON.stringify(data));
-this.data1 = [];
-for(let i=0; i<data.length; i++)  {
-    console.log('i==='+i);
-    console.log('datai==='+data[i].Subject);
-    console.log('i owner==='+data[i].Owner.Name);
-    
-    this.data1.push({ id: i, subject: data[i].Subject, owner: data[i].Owner.Name });
-                                    
+let dataWrapList = [];
+let i=0;
+data.forEach(itm => {
+let dWrap = {};
+dWrap.subject=itm.Subject;
+dWrap.tksubject='/'+itm.Id;
+dWrap.Description=itm.Description;
+dWrap.owner=itm.Owner.Name;
+dataWrapList.push(dWrap);
+});
+this.dataWrapperList = dataWrapList;
+/*for(let i=0; i<data.length; i++)  {
+console.log('i==='+i);
+console.log('datai==='+data[i].Subject);
+console.log('i owner==='+data[i].Owner.Name);
+
+this.data1.push({ id: i+1, subject: data[i].Subject, owner: data[i].Owner.Name });
+                                
 } 
-this.updateditems = this.data1;  
-console.log('updated=='+JSON.stringify(this.updateditems));
+this.updateditems = this.data1;  */
+console.log('updated=='+JSON.stringify(this.dataWrapperList));
+
 }else{
 
 console.log(error);
@@ -40,35 +52,34 @@ console.log(error);
 
 
 }
-    items = this.updateditems;
-    columns = columns;
-    defaultSortDirection = 'asc';
-    sortDirection = 'asc';
-    sortedBy;
+    data=this.dataWrapperList;
+defaultSortDirection = 'asc';
+sortDirection = 'asc';
+sortedBy;
 sortBy(field, reverse, primer) {
-    const key = primer
-        ? function (x) {
-              return primer(x[field]);
-          }
-        : function (x) {
-              return x[field];
-          };
+const key = primer
+    ? function (x) {
+            return primer(x[field]);
+        }
+    : function (x) {
+            return x[field];
+        };
 
-    return function (a, b) {
-        a = key(a);
-        b = key(b);
-        return reverse * ((a > b) - (b > a));
-    };
+return function (a, b) {
+    a = key(a);
+    b = key(b);
+    return reverse * ((a > b) - (b > a));
+};
 }
 
 onHandleSort(event) {
-    const { fieldName: sortedBy, sortDirection } = event.detail;
-    const cloneData = [...this.data];
+const { fieldName: sortedBy, sortDirection } = event.detail;
+const cloneData = [...this.dataWrapperList];
 
-    cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
-    this.data = cloneData;
-    this.sortDirection = sortDirection;
-    this.sortedBy = sortedBy;
+cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
+this.dataWrapperList = cloneData;
+this.sortDirection = sortDirection;
+this.sortedBy = sortedBy;
 }
 
 }
